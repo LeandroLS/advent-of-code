64a2ddf92a185d7984cc006442a9eef324ffa411
Leandro Lima <leandro.lsilva@pagar.me>
2025-07-24 15:53:42 -0300

    solving day02 part 1

---
+++ 2024/day-2/index.ts
@@ -0,0 +1,68 @@
+import { reportsList } from "./inputData.js";
+
+let correctReport = 0;
+function getDiff(lastNumber: number, currentNumber: number): number {
+  let diff = 0;
+  if (lastNumber > currentNumber) {
+    diff = lastNumber - currentNumber;
+  }
+  if (lastNumber < currentNumber) {
+    diff = currentNumber - lastNumber;
+  }
+  return diff;
+}
+
+function isIncreasing(
+  lastNumber: number,
+  currentNumber: number
+): boolean {
+  if (lastNumber > currentNumber) {
+    return true;
+  }
+  return false;
+}
+
+function isReportNumbersSafe(reportNumbers: number[]): boolean {
+  let isNumbersIncreasing: boolean = false;
+  let isNumbersDecreasing: boolean = false;
+  let isSafeReport: boolean = true;
+
+  const unsafeNumber = reportNumbers.find((num, index) => {
+    if (index === 0) {
+      return;
+    }
+    if (isIncreasing(reportNumbers[index - 1], num)) {
+      isNumbersIncreasing = true;
+    }
+    if (!isIncreasing(reportNumbers[index - 1], num)) {
+      isNumbersDecreasing = true;
+    }
+    if (reportNumbers[index - 1] === num) {
+      return true;
+    }
+    if (isNumbersIncreasing && isNumbersDecreasing) {
+      return true;
+    }
+    const diff = getDiff(reportNumbers[index - 1], num);
+    if (diff > 3) {
+      return true;
+    }
+  });
+
+  if (unsafeNumber) {
+    isSafeReport = false;
+  }
+
+  return isSafeReport;
+}
+
+reportsList.forEach((report) => {
+  const numbers = report.split(" ").map(Number);
+  console.log(`Processing report: ${numbers}`);
+  const isSafe = isReportNumbersSafe(numbers);
+  if (isSafe) {
+    correctReport++;
+  }
+});
+
+console.log(`The number of correct reports is: ${correctReport}`);
